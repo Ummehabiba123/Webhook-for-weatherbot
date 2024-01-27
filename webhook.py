4161641469d8bccf0f4a612ef4d5f8c3e51448c1
Ummehabiba123 <95669079+Ummehabiba123@users.noreply.github.com>
2024-01-27 21:34:03 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -36,7 +36,7 @@ def makeResponse(req):
         "displayText": speech,
         "source": "apiai-weather-webhook"
     }
-if __name__=="'__main__":
+if __name__=='__main__':
     port=int(os.getenv('PORT', 5000))
     print("starting app on port %d"  %port)
     app.run(debug=False, port=port, host='0.0.0.0')
